11e0f10aff2b1da77caeb776bec0cb92857bb83e
gayathiriarul <gayathiriarul2007@gmail.com>
2026-07-22 09:06:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.73%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def spiralOrder(self, matrix):
+        ans = []
+
+        while matrix:
+            ans += matrix.pop(0)
+
+            for row in matrix:
+                if row:
+                    ans.append(row.pop())
+
+            if matrix:
+                ans += matrix.pop()[::-1]
+
+            for row in matrix[::-1]:
+                if row:
+                    ans.append(row.pop(0))
+
+        return ans
