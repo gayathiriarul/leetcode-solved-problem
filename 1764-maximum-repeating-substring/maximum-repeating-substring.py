f76203ccc9b4b16e27898d41759cb95d45ddfdcb
gayathiriarul <gayathiriarul2007@gmail.com>
2026-08-26 13:58:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.70%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxRepeating(self, sequence, word):
+        k = 0
+        repeated_word = ""
+        while repeated_word + word in sequence:
+            repeated_word += word
+            k += 1
+        return k
