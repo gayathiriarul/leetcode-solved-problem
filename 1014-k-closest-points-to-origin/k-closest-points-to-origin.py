091a448f50161af37db5317db402c86165a2aeff
gayathiriarul <gayathiriarul2007@gmail.com>
2026-09-04 14:07:38 +0530

    Time: 58 ms (66.67%) | Memory: 24.8 MB (51.09%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,8 @@
+import heapq
+
+class Solution:
+    def kClosest(self, points, k):
+        return heapq.nsmallest(
+            k, points,
+            key=lambda p: p[0] ** 2 + p[1] ** 2
+        )
