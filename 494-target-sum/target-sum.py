d80c39cabf71149141f582c3d21550f609eb220f
gayathiriarul <gayathiriarul2007@gmail.com>
2026-09-07 11:58:48 +0530

    Time: 91 ms (69.19%) | Memory: 19.4 MB (74.81%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTargetSumWays(self, nums, target):
+        dp = {0: 1}
+
+        for num in nums:
+            new_dp = {}
+
+            for total, count in dp.items():
+                new_dp[total + num] = new_dp.get(total + num, 0) + count
+                new_dp[total - num] = new_dp.get(total - num, 0) + count
+
+            dp = new_dp
+
+        return dp.get(target, 0)
