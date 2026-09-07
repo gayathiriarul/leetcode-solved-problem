9996a100c1f86e4e605fb9db3e7cf1166c87565d
gayathiriarul <gayathiriarul2007@gmail.com>
2026-09-07 12:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (27.88%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findRedundantConnection(self, edges):
+        parent = list(range(len(edges) + 1))
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(a, b):
+            rootA = find(a)
+            rootB = find(b)
+
+            if rootA == rootB:
+                return False
+
+            parent[rootA] = rootB
+            return True
+
+        for a, b in edges:
+            if not union(a, b):
+                return [a, b]
+
+        return []
